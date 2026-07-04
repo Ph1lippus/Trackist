bae8cfcb27ac574c96c848ce1d627f69b8442992
Ph1lippus <philippus.sanctus@gmail.com>
2026-07-04 19:45:46 +0100

    Removing loading since its just giving more problems

--- scripts.js
+++ scripts.js
@@ -3,16 +3,6 @@ const supabaseKey = 'sb_publishable_unnBNQ4MtAtOWxq7_bJU9w_mbeJwIKE';
 
 const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-const loading = document.getElementById('loadingScreen');
-if (loading) {
-    setTimeout(function() {
-        loading.style.opacity = '0';
-        setTimeout(function() {
-            loading.style.display = 'none';
-        }, 600);
-    }, 800);
-}
-
 const backBtn = document.getElementById('backToTop');
 if (backBtn) {
     backBtn.addEventListener('click', function() {
