1da150dac5101a8d875b54d8356bd6a36a922bc8
Ph1lippus <philippus.sacntus@gmail.com>
2026-07-04 16:06:14 +0100

    Added connection to supabase

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loading = document.getElementById('loadingScreen');
-    if (loading) {
+const supabaseUrl = 'https://iqlzdmjamsvxinqbrnix.supabase.co/rest/v1/';
+const supabaseKey = 'sb_publishable_unnBNQ4MtAtOWxq7_bJU9w_mbeJwIKE';
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+const loading = document.getElementById('loadingScreen');
+if (loading) {
         setTimeout(function() {
             loading.style.opacity = '0';
             setTimeout(function() {
                 loading.style.display = 'none';
             }, 600);
         }, 800);
-    }
+}
 
-    const backBtn = document.getElementById('backToTop');
-    if (backBtn) {
+const backBtn = document.getElementById('backToTop');
+if (backBtn) { 
         backBtn.addEventListener('click', function() {
             window.scrollTo({
                 top: 0,
                 behavior: 'smooth'
             });
         });
-    }
-
-    
-});
+}
 
 
