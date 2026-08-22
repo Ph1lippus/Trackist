14b72c1acd1d899bb76dbde7585ef6cf1d5ad200
Ph1lippus <philippus.sanctus@gmail.com>
2026-08-22 01:21:53 +0100

    fix: show filled bookmark icon for watchlisted items when onAdd is provided

--- src/components/media/MediaCard.tsx
+++ src/components/media/MediaCard.tsx
@@ -115,7 +115,7 @@ const MediaCard: React.FC<MediaCardProps> = ({
         },
         [onDelete, item],
     )
-    const showAddButton = !compact && onAdd && !isInWatchlist && !listMode
+    const showAddButton = !compact && onAdd && !listMode
     const showAddToListButton = !compact && onAddToList && !isPerson && !listMode
     const showMarkWatched = !compact && !isPerson && onMarkWatched && !onMarkUnwatched
     const showMarkUnwatched = !compact && !isPerson && onMarkUnwatched && !onMarkWatched
